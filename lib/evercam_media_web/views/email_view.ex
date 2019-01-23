55c2428eb25e16338b7f2ffb4819cce24a7a1857
Azhar Malik <azharmalik02@gmail.com>
2019-01-23 15:51:36 +0500

    Added function to check thumbnail exists or not

--- lib/evercam_media_web/views/email_view.ex
+++ lib/evercam_media_web/views/email_view.ex
@@ -5,6 +5,13 @@ defmodule EvercamMediaWeb.EmailView do
     "#{user.firstname} #{user.lastname}"
   end
 
+  def image_tag(has_thumbnail) do
+    case has_thumbnail do
+      true -> "<br><img src='cid:snapshot.jpg' alt='Camera Preview' style='width: 100%; display:block; margin:0 auto;' >"
+      _ -> ""
+    end
+  end
+
   def get_user_name(email) do
     email
     |> User.by_username_or_email
